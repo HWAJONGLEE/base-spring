e854922f84b1eb926fbc6c174a12fe64bcf7e1d8
HWAJONG LEE <wowlover10@nate.com>
2022-02-27 18:45:18 +0900

    add jdbc old code, connection H2DB

--- src/main/java/com/learn/basespring/service/SpringConfig.java
+++ src/main/java/com/learn/basespring/service/SpringConfig.java
@@ -1,14 +1,24 @@
 package com.learn.basespring.service;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.learn.basespring.repository.JdbcMemberRepository;
 import com.learn.basespring.repository.MemberRepository;
-import com.learn.basespring.repository.MemoryMemberRepository;
-
+//di 주입방식중에 하나 @Repository 어노테이션 없이 bean 객체 생성
 @Configuration
 public class SpringConfig {
 
+	private DataSource dataSource;
+	
+	@Autowired
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
 	@Bean
 	public MemberService memberService() {
 		return new MemberService(memberRepository());
@@ -16,6 +26,7 @@ public class SpringConfig {
 	
 	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+//		return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);
 	}
 }
